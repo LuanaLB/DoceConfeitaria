cfb5512fb88e18fa68c1fa0c7b9f9cf9a2bf01d5
Luana Batista <97992822+LuanaLB@users.noreply.github.com>
2023-08-14 09:26:46 -0300

    mudanca

---
+++ navbar.js
@@ -0,0 +1,10 @@
+/*const navbarContainer = document.getElementById("navbarContainer");
+
+fetch("nav.html")
+  .then(response => response.text())
+  .then(data => {
+    navbarContainer.innerHTML = data;
+  })
+  .catch(error => {
+    console.error("Erro ao carregar a barra de navegação:", error);
+  });*/
